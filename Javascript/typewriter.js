2fa370f605a68ebcc889f03a5113a654808afdc5
XainMuhammadKhan <xain.k19@gmail.com>
2025-03-21 15:18:38 +0500

    -

---
+++ Javascript/typewriter.js
@@ -0,0 +1,64 @@
+class TypeWriter {
+    constructor(txtElement, words, wait = 3000) {
+        this.txtElement = txtElement;
+        this.words = words;
+        this.txt = '';
+        this.wordIndex = 0;
+        this.wait = parseInt(wait, 10);
+        this.type();
+        this.isDeleting = false;
+    }
+
+    type() {
+        // Get full word of current word
+        const current = this.wordIndex % this.words.length;
+        const fullTxt = this.words[current];
+
+        // Check if deleting
+        if(this.isDeleting) {
+            // Remove char
+            this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+            // Add char
+            this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        // Insert txt into element
+        this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+        // Initial type speed
+        let typeSpeed = 100;
+
+        if(this.isDeleting) {
+            typeSpeed /= 2;
+        }
+
+        // If word is complete
+        if(!this.isDeleting && this.txt === fullTxt) {
+            // Make pause at end
+            typeSpeed = this.wait;
+            this.isDeleting = true;
+        } else if(this.isDeleting && this.txt === '') {
+            this.isDeleting = false;
+            // Move to next word
+            this.wordIndex++;
+            // Pause before start typing
+            typeSpeed = 500;
+        }
+
+        setTimeout(() => this.type(), typeSpeed);
+    }
+}
+
+// Init On DOM Load
+document.addEventListener('DOMContentLoaded', init);
+
+// Init App
+function init() {
+    const txtElement = document.querySelector('.dynamic-txts');
+    const words = ['Flutter Developer', 'Web Developer', 'Graphic Designer', 'AI Engineer', 'Cybersecurity Engineer'];
+    const wait = 3000;
+    
+    // Init TypeWriter
+    new TypeWriter(txtElement, words, wait);
+} 
